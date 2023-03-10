0e8fa36b8cd0be240fce69076561d449fc481232
Kevin Meister <kevin.meister@makrofactory.com>
2023-03-10 20:08:56 +0100

    kurs app bis lektion 60

--- angular/courseapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/courseapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  recipes: Recipe[] = [
+    new Recipe("Kartoffelpizza", "Backe Backe PIZZA, hahahaha", "https://img.chefkoch-cdn.de/rezepte/1028531208071533/bilder/364565/crop-642x428/kartoffelpizza.jpg"),
+    new Recipe("Kartoffelpizza", "Backe Backe PIZZA, hahahaha", "https://img.chefkoch-cdn.de/rezepte/1028531208071533/bilder/364565/crop-642x428/kartoffelpizza.jpg")
 
+  ];
 }
